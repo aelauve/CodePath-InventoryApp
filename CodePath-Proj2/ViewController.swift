611eb8538ac647b25980b5a4af435e96f095e9d5
aedwards4 <aedwards4@hawk.iit.edu>
2021-02-20 18:55:18 -0600

    Initial Commit

---
+++ CodePath-Proj2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodePath-Proj2
+//
+//  Created by Alexis Edwards on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
