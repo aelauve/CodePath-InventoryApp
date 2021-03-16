5fbff571f2ef7fe230f07c0d60c019e309713008
aedwards4 <aedwards4@hawk.iit.edu>
2021-03-16 17:41:39 -0500

    Added outlets for LoginVC elements

--- CodePath-Proj2/LoginViewController.swift
+++ CodePath-Proj2/LoginViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
