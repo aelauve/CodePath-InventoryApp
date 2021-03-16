49ecb314494d65e5be28acdc08f126d466ef5e55
aedwards4 <aedwards4@hawk.iit.edu>
2021-03-16 17:06:28 -0500

    Added Firebase import

--- CodePath-Proj2/AppDelegate.swift
+++ CodePath-Proj2/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+
         return true
     }
 
@@ -30,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
 
 
 }
