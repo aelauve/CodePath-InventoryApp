9d0d486dbd614c5143a7ba3792d9e2543cb25a83
aedwards4 <aedwards4@hawk.iit.edu>
2021-03-30 09:02:53 -0500

    unknown

---
+++ CodePath-Proj2/AddItemViewController.swift
@@ -0,0 +1,132 @@
+//
+//  AddItemViewController.swift
+//  CodePath-Proj2
+//
+//  Created by Alexis Edwards on 2/20/21.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var addToInventoryButton: UIButton!
+    //ignore next line
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    @IBOutlet weak var nameTextBox: UITextField!
+    @IBOutlet weak var amountTextBox: UITextField!
+    @IBOutlet weak var expirationSwitch: UISwitch!
+    @IBOutlet weak var expirationDate: UIDatePicker!
+    @IBOutlet weak var notesTextBox: UITextView!
+    
+    //Still need to add actions for the buttons
+    //Also need to add text fields
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // **** Set itemImage to default image for that category!
+        
+        //Temporary styling of item image
+        itemImageView.layer.cornerRadius = (itemImageView?.frame.size.width ?? 0.0) / 2
+        itemImageView.layer.borderWidth = 2.0
+        itemImageView.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        itemImageView.layer.backgroundColor = #colorLiteral(red: 1, green: 0.5132452846, blue: 0.6042660475, alpha: 1)
+        
+        //Styling border of Name and Amount text boxes
+        nameTextBox.layer.borderWidth = 1.0
+        nameTextBox.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        nameTextBox.layer.cornerRadius = 5
+        
+        amountTextBox.layer.borderWidth = 1.0
+        amountTextBox.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        amountTextBox.layer.cornerRadius = 5
+        
+        //Styling '+' and '-' buttons
+        plusButton.layer.cornerRadius = 5
+        plusButton.layer.borderWidth = 1.0
+        plusButton.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        
+        minusButton.layer.cornerRadius = 5
+        minusButton.layer.borderWidth = 1.0
+        minusButton.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        
+        //Styling Notes text view
+        notesTextBox.layer.borderWidth = 1.0
+        notesTextBox.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        notesTextBox.layer.cornerRadius = 5
+        
+        //Styling 'Add to' Button
+        addToInventoryButton.layer.cornerRadius = 10
+        addToInventoryButton.layer.borderWidth = 2.0
+        addToInventoryButton.layer.borderColor = #colorLiteral(red: 1, green: 0.3807129264, blue: 0.4381764233, alpha: 1)
+        addToInventoryButton.layer.backgroundColor = #colorLiteral(red: 1, green: 0.5132452846, blue: 0.6042660475, alpha: 1)
+        
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func decrementAmount(_ sender: Any) {
+        var count = Int(amountTextBox.text!)
+        if count! > 1{
+            count = count! - 1
+            amountTextBox.text = String(count!)
+        }
+    }
+    
+    @IBAction func incrementAmount(_ sender: Any) {
+        var count = Int(amountTextBox.text!)
+        count = count! + 1
+        amountTextBox.text = String(count!)
+    }
+    
+    @IBAction func expirationToggled(_ sender: Any) {
+        if expirationSwitch.isOn == true{
+            expirationDate.isEnabled = true
+        }
+        else {
+            expirationDate.isEnabled = false
+        }
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addToInventoryClicked(_ sender: Any) {
+        
+        // If input is blank, show error message
+        if nameTextBox.text == ""{
+            let alert = UIAlertController(title: "Uh Oh!", message: "Name must be filled in.", preferredStyle: UIAlertController.Style.alert)
+            let alertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+            {
+                (UIAlertAction) -> Void in
+            }
+            alert.addAction(alertAction)
+            present(alert, animated: true)
+            {
+                () -> Void in
+            }
+            
+        } else {
+            //Add item to category in user database
+            print("Add success")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CodePath-Proj2/ShoppingListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ShoppingListViewController.swift
+//  CodePath-Proj2
+//
+//  Created by Alexis Edwards on 2/21/21.
+//
+
+import UIKit
+
+class ShoppingListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var listTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        listTableView.delegate = self
+        listTableView.dataSource = self
+        
+        listTableView.estimatedRowHeight = 0
+        
+        listTableView.reloadData()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Function for what to do when user taps a cell
+        //May not be needed, just placed sample code in case
+        print("you tapped me")
+        
+        //Trying to change the button imagew when a cell is selected
+//        let cell = listTableView.dequeueReusableCell(withIdentifier: "listItemCell", for: indexPath) as! ShopListItemsTableViewCell
+//
+//        cell.checkOffButton.setImage(UIImage(named: "circle.fill"), for: .selected)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listTableView.dequeueReusableCell(withIdentifier: "listItemCell", for: indexPath) as! ShopListItemsTableViewCell
+        
+        cell.listItemImage.layer.borderWidth = 1.0
+        cell.listItemImage.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        cell.listItemImage.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        cell.listItemImage.layer.cornerRadius = (cell.listItemImage?.frame.size.width ?? 0.0) / 2
+        
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
