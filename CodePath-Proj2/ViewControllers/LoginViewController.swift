2090e9219d05db6244e94c9d59e0dbff7f7851b5
aedwards4 <aedwards4@hawk.iit.edu>
2021-04-20 18:00:02 -0500

    Added color sets

--- CodePath-Proj2/ViewControllers/LoginViewController.swift
+++ CodePath-Proj2/ViewControllers/LoginViewController.swift
@@ -15,11 +15,13 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
     
+    let colorPaletteReg = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loginButton.layer.backgroundColor = #colorLiteral(red: 1, green: 0.5132452846, blue: 0.6042660475, alpha: 1)
+        loginButton.layer.backgroundColor = colorPaletteReg as? CGColor
         loginButton.layer.borderColor = #colorLiteral(red: 0.852301836, green: 0.4426146448, blue: 0.608592689, alpha: 1)
         loginButton.layer.cornerRadius = 10
         
